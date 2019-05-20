56394b2e1ae0bb27834f8e0bb0995b8eca0165fc
bluelogix <bluelogix010@gmail.com>
2019-05-20 17:21:15 -0400

    depth first search done

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -43,7 +43,15 @@ class Graph:
         Print each vertex in depth-first order
         beginning from starting_vertex.
         """
-        pass  # TODO
+        stack = Stack()
+        stack.push(starting_vertex)
+        visited = set()
+        while stack.size() > 0:
+            vertex = stack.pop()
+            if vertex not in visited:
+                visited.add(vertex)
+                for next_vertex in self.vertices[vertex]:
+                    stack.push(next_vertex)
 
     def dft_recursive(self, starting_vertex):
         """
